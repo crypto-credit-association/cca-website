faa9aef6340855ad4bb5566c8ec1da22b5bec9c2
Elektra Murphy <elektra.murphy@icloud.com>
2022-09-25 13:28:45 +0100

    Removed ED section

--- pages/board-members.js
+++ pages/board-members.js
@@ -18,25 +18,21 @@ function BoardMembersPage() {
             <div className="bg-gray">
                 <div className="max-w-7xl m-auto px-5 lg:px-0 py-16">
                 <p className="pb-10 lg:pb-24 pt-0 lg:pt-10 text-2xl font-base">The CCA is governed by an elected Board of Directors. The Directors are responsible for governance and planning of all Association activity. Over time we expect special interest committees to form and focus on specific aspects of this activity.</p>
-                    <h3 className="pb-6 text-3xl font-bold">Executive Directors</h3>
-                    <div className="border-b-2 border-gold w-64 mb-10"></div>
-                    <div className="grid grid-cols-1 gap-10 md:grid-cols-3">
-                        <BoardMembers
-                            img="/images/george.png"
-                            name="George Maddox"
-                            title="Executive Director"
-                            bio=" George is a bilingual 20-year resident of Tokyo, who launched his Financial Services career as Controller at Credit Suisse, UBS and Merrill Lynch, following a successful foray in marketing & advertising for Formula 1 Racing's Benetton Team." />
-                    </div>
                     <h3 className="pb-6 text-3xl font-bold pt-12">Board Members</h3>
                     <div className="border-b-2 border-gold w-64 mb-12"></div>
 
                     <div className="grid grid-cols-1 gap-10 md:grid-cols-3">
-                        <BoardMembers
+                    <BoardMembers
                             img="/images/paul-murphy.png"
                             name="Paul Murphy"
+                            title="Chair"
+                            bio="Co-Founder of Credmark, which was originally formed to provide credit scores for Ethereum addresses. One of Paul’s startups was acquired by FICO. He knows more about consumer credit and corporate ratings that any normal person should."
+                            bioTwo="Paul lives in Italy where he tries to balance his love of crypto data with his love of olive oil and cheese." />
+                    <BoardMembers
+                            img="/images/george-maddock.png"
+                            name="George Maddock"
                             title="Board Member"
-                            bio=" Born in Argentina, Paul has lived and worked all over the world. He used to write a lot of code. Now he’s kind of rusty. He does whatever needs doing, whether or not it’s fun."
-                            bioTwo="He believes the answer to most questions can be found in musical and opera scores." />
+                            bio=" George is a bilingual 20-year resident of Tokyo, who launched his Financial Services career as Controller at Credit Suisse, UBS and Merrill Lynch, following a successful foray in marketing & advertising for Formula 1 Racing's Benetton Team." />
                         <BoardMembers
                             img="/images/john-salomon.png"
                             name="John Salomon"
@@ -49,7 +45,6 @@ function BoardMembersPage() {
                             bio="Darshan is the founder of X-Margin, a Zero-Knowledge Cross Margin Solution for trading firms. His experience ranges from options trading in Cryptocurrency and strong programming skills."
                             bioTwo="He is also the Founder of Well Fed Foundation - a food redistribution charity using technology." />
                     </div>
-                    <GetinTouch />
                 </div>
             </div>
             <Footer />
