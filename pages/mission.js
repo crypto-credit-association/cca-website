5e61890bb146161d9dc276e07f43c7d8fe097f61
Elektra Murphy <elektra.murphy@icloud.com>
2022-09-25 13:43:06 +0100

    Mission page copy update

--- pages/mission.js
+++ pages/mission.js
@@ -17,7 +17,7 @@ function Mission() {
             <div className="bg-gray">
                 <div className="max-w-7xl m-auto px-5 lg:px-12 py-12">
                     <div>
-                        <h3 className="pb-6 text-3xl font-bold">Our Vision</h3>
+                        <h3 className="pb-6 text-3xl font-bold">Our Goals</h3>
                         <div className="border-b-2 border-gold w-48"></div>
                         <ul className="ml-5 md:ml-0 py-6 list-outside list-disc">
                             <li>Define and publish standards.</li>
@@ -25,26 +25,15 @@ function Mission() {
                             <li>Work with regulatory bodies to encourage these bodies to work together to ensure consistent rules.</li>
                         </ul>
                     </div>
-                    <div className="grid grid-cols-1 md:grid-cols-3">
+                    <div className="grid grid-cols-1 md:grid-cols-2">
                         <div>
                             <h3 className="pt-12 pb-6 text-3xl font-bold">Standards</h3>
                             <div className="border-b-2 border-gold w-56"></div>
                             <p className="py-6">CCA sub-committees will work with Members to define standards in three key  areas as data and the market value/need differ significantly:</p>
                             <ul className="list-inside list-disc">
-                                <li>consumer credit</li>
-                                <li>private company credit, and</li>
-                                <li>public company ratings</li>
+                                <li>Consumer credit</li>
+                                <li>Corporate ratings</li>
                             </ul>
-                        </div>
-                        <div>
-                            <h3 className="pt-12 pb-6 text-3xl font-bold">Audit</h3>
-                            <div className="border-b-2 border-gold w-56"></div>
-                            <p className="py-6">Certain processes defined by the CCA will require third-party audit and certification services.The CCA will publish references to companies capable of providing these services.</p>
-                            <Link href="/audits" rel="noreferrer"><p className="cursor-pointer flex font-semibold">Find out more<img
-                                className="ml-2"
-                                src="/svg/arrow-blue.svg"
-                                alt="Download icon"
-                            /></p></Link>
                         </div>
                         <div className="md:mx-12">
                             <h3 className="pt-12 pb-6 text-3xl font-bold">Regulatory</h3>
