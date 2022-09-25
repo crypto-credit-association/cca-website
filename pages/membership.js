aae838e932a5fb10f410e27f74c7860c3d4315bf
Elektra Murphy <elektra.murphy@icloud.com>
2022-09-25 13:35:22 +0100

    Membership page update

--- pages/membership.js
+++ pages/membership.js
@@ -16,9 +16,9 @@ function Membership() {
         </div>
       </div>
       <div className="bg-gray">
-        <div className="py-12 max-w-7xl m-auto">
+        <div className="py-12 lg:py-32 max-w-7xl m-auto">
           <div className="mx-5 md:mx-12">
-            <h3 className="pb-12 text-3xl font-bold underline">Why join</h3>
+            <h3 className="pb-12 text-3xl font-bold underline">Why join?</h3>
             <div className="flex flex-wrap justify-between mb-24">
               <div className="border-l-2 border-gold pr-6 pt-6">
                 <img
@@ -32,41 +32,25 @@ function Membership() {
                   className="ml-6 w-8"
                   src="/svg/blue-tick.svg"
                   alt="Tick icon"
-                /><p className="ml-6 pb-6 text-base mt-2">Inform and influence regulation.</p>
+                /><p className="ml-6 pb-6 text-base mt-2">Access industry resources, including certification programs.
+                </p>
               </div>
               <div className="border-l-2 border-gold pr-6 pt-6">
                 <img
                   className="ml-6 w-8"
                   src="/svg/blue-tick.svg"
                   alt="Tick icon"
-                /><p className="ml-6 pb-6 text-base mt-2">Access industry materials and certification programs.
-                </p>
+                /><p className="ml-6 pb-6 text-base mt-2">Inform and influence regulation.</p>
               </div>
               </div>
               <p className="pb-6 text-xl font-semibold underline">CCA services:</p>
               <p className="pb-6">The CCA is a not-for-profit industry association dedicated to improving the robustness of the crypto-lending industry. CCA uses member fees and contributions to provide a number of services, including.</p>
               <ul className="ml-5 md:ml-0 list-outside list-disc px-5">
-                    <li>Relevant research materials and publications</li>
-                    <li>Member-accessible research portal</li>
-                    <li>Meeting and conference services</li>
-                    <li>Facilitation of information exchange between participants:  online collaboration, working groups and committees, regular briefing calls</li>
+                    <li>Research and publications</li>
+                    <li>Member-accessible research portal (coming soon)</li>
+                    <li>Meetings and conferences</li>
+                    <li>Facilitation of information exchange between participants: online collaboration, working groups and committees, regular briefing calls</li>
                   </ul>
-                  <Link
-                            href="/statutes-and-rules"
-                        >
-                            <button
-                                type="button"
-                                href="/memberships"
-                                className="my-10 items-center bg-blue-dark px-10 inline-flex py-3 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white"
-                            >
-                                Learn more about our statutes and rules
-                                <img
-                                className="ml-2"
-                                src="/svg/arrow-white.svg"
-                                alt="Download icon"
-                            />
-                            </button>
-                        </Link>
               <p className="pt-5 pb-6 text-xl font-semibold underline">Who should join?</p>
               <ul className="ml-5 md:ml-0 list-outside list-disc px-5 pb-12">
                     <li>Organisations:  any firm engaged in crypto lending.</li>
@@ -91,7 +75,6 @@ function Membership() {
                   <p className="py-6 px-5 font-semibold text-white">*Charter memberships will only be available in 2022. A Charter Member in good standing will always be recognized as such.</p>
                 </div>
             </div>
-            <GetinTouch />
           </div>
         </div>
       </div>
