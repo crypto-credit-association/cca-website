20d47c1aac143249368a7e08a6c8e1b0a2964fff
Elektra Murphy <elektra.murphy@icloud.com>
2022-10-02 13:24:38 +0100

    Membership page copy updates

--- pages/membership.js
+++ pages/membership.js
@@ -60,35 +60,37 @@ function Membership() {
                 <div className="bg-blue-dark h-full py-12 shadow-2xl mb-5 mx-5 md:mx-2">
                   <h4 className="text-white text-2xl font-semibold underline px-5">Individual Members</h4>
                   <p className="py-6 text-white px-5 italic">For single persons </p>
-                  <p className="text-white px-5">Yearly Membership Fee: US$ 225</p>
+                  <p className="text-white px-5">Membership Fee: US$ 225/year</p>
                   <p className="text-white px-5">Joining Fee: None</p>
                 </div>
                 <div className="bg-blue-dark h-full py-12 shadow-2xl mx-5 md:mx-2 mt-5 md:mt-0">
                   <h4 className="text-white text-2xl font-semibold underline px-5">Corporate Members</h4>
                   <p className="italic py-6 text-white px-5">For small firms (up to 1,000 employees)</p>
-                  <p className="text-white px-5">Yearly Membership Fee: US$ 2,250</p>
+                  <p className="text-white px-5">Membership Fee: US$ 2,250/year</p>
                   <p className="text-white px-5">Joining Fee: US$ 1,000</p>
                 </div>
                 <div className="bg-blue-dark mt-10 h-full py-12 shadow-2xl mx-5 md:mx-2  md:mt-0">
                   <h4 className="text-white text-2xl font-semibold underline px-5">Enterprise Members</h4>
                   <p className="py-6 text-white px-5 italic">For large firms (over 1,000 employees) </p>
-                  <p className="text-white px-5">Yearly Membership Fee: US$ 12,500</p>
+                  <p className="text-white px-5">Membership Fee: US$ 12,500/year</p>
                   <p className="text-white px-5">Joining Fee: US$ 2,500</p>
                 </div>
                 <div className="bg-blue-dark mt-10 h-full pt-12 pb-24 shadow-2xl mx-5 md:mx-2 md:mt-0">
                   <h4 className="text-white text-2xl font-semibold underline px-5">Charter Members</h4>
                   <p className="py-6 text-white px-5 italic">For any firm</p>
-                  <p className="text-white px-5">Yearly Membership Fee: US$ 22,500</p>
+                  <p className="text-white px-5">Membership Fee: US$ 22,500/year</p>
                   <p className="text-white px-5">Joining fee: US$ 5,000*</p>
-                  <p className="py-6 px-5 font-semibold text-white">*Charter memberships will only be available in 2022. A Charter Member in good standing will always be recognized as such.</p>
+                  <p className="py-6 px-5 font-semibold text-white">*Charter memberships will only be available until 1 June 2023. A Charter Member in good standing will always be recognized as such.</p>
                 </div>
             </div>
             <p className="pt-10 pb-6 text-xl font-semibold underline">About the membership:</p>
-                    <p>CCA activities are funded exclusively by membership dues.</p>
-                    <p className="py-2">Member dues are annual, and assessed on a voluntary basis. The CCA relies on its members’ understanding that a strong, coordinated set of capabilities supported by the crypto lending industry requires financial resources.</p>
-                    <p className="py-2">Fees are assessed annually, and are payable via bank or crypto transfer.  For organizations joining before 1 June 2023, joining fees are waived.</p>
-                    <p className="py-2">Corporations may nominate and vote for Board memberships annually.</p>
-                    <p className="py-2">Charter memberships will only be available until 1 June 2023. A Charter Member in good standing will always be recognized as such.</p>
+            <ul className="ml-5 md:ml-0 list-outside list-disc px-5">
+                    <li>CCA activities are funded exclusively by membership dues.</li>
+                    <li className="py-2">Member dues are annual, and assessed on a voluntary basis. The CCA relies on its members’ understanding that a strong, coordinated set of capabilities supported by the crypto lending industry requires financial resources.</li>
+                    <li className="py-2">Fees are assessed annually, and are payable via bank or crypto transfer.  For organizations joining before 1 June 2023, joining fees are waived.</li>
+                    <li className="py-2">Corporations may nominate and vote for Board memberships annually.</li>
+                    <li className="py-2">Charter memberships will only be available until 1 June 2023. A Charter Member in good standing will always be recognized as such.</li>
+            </ul>
           </div>
         </div>
       </div>
