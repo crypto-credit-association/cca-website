36fd22577abca0a3911afc41cdd3a442cca1affe
Paul Murphy <paul@clarify.io>
2023-04-21 13:12:56 +0200

    Updated all apostraphes and moved Darshan's bio up

--- pages/board-members.js
+++ pages/board-members.js
@@ -24,23 +24,23 @@ function BoardMembersPage() {
                             img="/assets/paul-murphy.png"
                             name="Paul Murphy"
                             title="Chair"
-                            bio="Co-Founder of Credmark, which was originally formed to provide credit scores for Ethereum addresses. One of Paul’s startups was acquired by FICO. He knows more about consumer credit and corporate ratings that any normal person should."
+        bio="Co-Founder of Credmark, which was originally formed to provide credit scores for Ethereum addresses. One of Paul&apos;s startups was acquired by FICO. He knows more about consumer credit and corporate ratings that any normal person should."
                             bioTwo="Paul lives in Italy where he tries to balance his love of crypto data with his love of olive oil and cheese." />
                     <BoardMembers
                             img="/assets/george-maddock.png"
                             name="George Maddock"
                             title="Board Member"
-                            bio=" George is a bilingual 20-year resident of Tokyo, who launched his Financial Services career as Controller at Credit Suisse, UBS and Merrill Lynch, following a successful foray in marketing & advertising for Formula 1 Racing’s Benetton Team." />
+                            bio=" George is a bilingual 20-year resident of Tokyo, who launched his Financial Services career as Controller at Credit Suisse, UBS and Merrill Lynch, following a successful foray in marketing & advertising for Formula 1 Racing&apos;s Benetton Team." />
+                        <BoardMembers
+                            img="/assets/darshain.png"
+                            name="Darshan Vaidya"
+                            title="Board Member"
+                            bio="Darshan is co-founder of Credora, a technology firm that is leveraging privacy-preserving technology to build confidence in credit markets. Credora enables real-time credit analytics and powers transparent and efficient markets. Darshan&apos;s background is from derivatives trading and risk management, having worked for 12 years across investment banks and market making firms in traditional and crypto markets." />
                         <BoardMembers
                             img="/assets/john.png"
                             name="John Salomon"
                             title="Board Member"
                             bio="John Salomon is an information security professional with over 25 years of international, cross-sector experience in technology and leadership roles.  His in-depth knowledge of security risk management and regulatory considerations in the global financial sector includes a strong understanding of the challenges around the increasing prevalence of fintech and crypto services as established parts of the industry.  John is a member of the Cybersecurity Advisory Network (CyAN); he is a Swiss national and resides in Spain." />
-                        <BoardMembers
-                            img="/assets/darshain.png"
-                            name="Darshan Vaidya"
-                            title="Board Member"
-                            bio="Darshan is co-founder of Credora, a technology firm that is leveraging privacy-preserving technology to build confidence in credit markets. Credora enables real-time credit analytics and powers transparent and efficient markets. Darshan’s background is from derivatives trading and risk management, having worked for 12 years across investment banks and market making firms in traditional and crypto markets."
                     </div>
                 </div>
             </div>
