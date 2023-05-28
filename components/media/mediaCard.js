b7447aac0446ee494555c648cb53f5e3fbdb430b
Elektra Murphy <elektra.murphy@icloud.com>
2023-05-28 14:42:45 +0100

    Class updated to classname

--- components/media/mediaCard.js
+++ components/media/mediaCard.js
@@ -1,88 +1,88 @@
 export default function MediaCard() {
     return (
-        <div class="divide-y divide-gray-200 overflow-hidden rounded-lg bg-gray-200 shadow sm:grid sm:grid-cols-2 sm:gap-px sm:divide-y-0">
-            <div class="rounded-tl-lg sm:rounded-tr-none group relative bg-white p-6">
+        <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-gray-200 shadow sm:grid sm:grid-cols-2 sm:gap-px sm:divide-y-0">
+            <div className="rounded-tl-lg sm:rounded-tr-none group relative bg-white p-6">
                 <div>
-                    <span class="inline-flex rounded-lg">
-                        <svg fill="#5e7591" viewBox="0 0 24 24" class="h-12 w-12" aria-hidden="true"><path fill-rule="evenodd" d="M19.812 5.418c.861.23 1.538.907 1.768 1.768C21.998 8.746 22 12 22 12s0 3.255-.418 4.814a2.504 2.504 0 0 1-1.768 1.768c-1.56.419-7.814.419-7.814.419s-6.255 0-7.814-.419a2.505 2.505 0 0 1-1.768-1.768C2 15.255 2 12 2 12s0-3.255.417-4.814a2.507 2.507 0 0 1 1.768-1.768C5.744 5 11.998 5 11.998 5s6.255 0 7.814.418ZM15.194 12 10 15V9l5.194 3Z" clip-rule="evenodd"></path></svg>
+                    <span className="inline-flex rounded-lg">
+                        <svg fill="#5e7591" viewBox="0 0 24 24" className="h-12 w-12" aria-hidden="true"><path fill-rule="evenodd" d="M19.812 5.418c.861.23 1.538.907 1.768 1.768C21.998 8.746 22 12 22 12s0 3.255-.418 4.814a2.504 2.504 0 0 1-1.768 1.768c-1.56.419-7.814.419-7.814.419s-6.255 0-7.814-.419a2.505 2.505 0 0 1-1.768-1.768C2 15.255 2 12 2 12s0-3.255.417-4.814a2.507 2.507 0 0 1 1.768-1.768C5.744 5 11.998 5 11.998 5s6.255 0 7.814.418ZM15.194 12 10 15V9l5.194 3Z" clip-rule="evenodd"></path></svg>
                     </span>
                 </div>
-                <div class="mt-2">
-                    <h3 class="text-base font-semibold leading-6 text-gray-900">
-                        <a href="https://www.youtube.com/@cryptocredit" target="_blank" rel="noreferrer" class="focus:outline-none">
-                            <span class="absolute inset-0" aria-hidden="true"></span>
+                <div className="mt-2">
+                    <h3 className="text-base font-semibold leading-6 text-gray-900">
+                        <a href="https://www.youtube.com/@cryptocredit" target="_blank" rel="noreferrer" className="focus:outline-none">
+                            <span className="absolute inset-0" aria-hidden="true"></span>
                             YouTube
                         </a>
                     </h3>
-                    <p class="mt-2 text-sm text-gray-500">Explore our YouTube channel and gain valuable knowledge about crypto credit, blockchain technology, lending protocols, decentralized finance (DeFi), and much more. Our videos are carefully crafted to provide clear explanations, real-life examples, and expert perspectives, making complex concepts accessible to all.</p>
+                    <p className="mt-2 text-sm text-gray-500">Explore our YouTube channel and gain valuable knowledge about crypto credit, blockchain technology, lending protocols, decentralized finance (DeFi), and much more. Our videos are carefully crafted to provide clear explanations, real-life examples, and expert perspectives, making complex concepts accessible to all.</p>
                 </div>
-                <span class="pointer-events-none absolute right-6 top-6 text-gray-300 group-hover:text-gray-400" aria-hidden="true">
-                    <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                <span className="pointer-events-none absolute right-6 top-6 text-gray-300 group-hover:text-gray-400" aria-hidden="true">
+                    <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
                         <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
                     </svg>
                 </span>
             </div>
-            <div class="sm:rounded-tr-lg group relative bg-white p-6">
+            <div className="sm:rounded-tr-lg group relative bg-white p-6">
                 <div>
-                    <span class="inline-flex rounded-lg">
-                        <svg fill="#5e7591" viewBox="0 0 24 24" class="h-12 w-12" aria-hidden="true"><path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path></svg>
+                    <span className="inline-flex rounded-lg">
+                        <svg fill="#5e7591" viewBox="0 0 24 24" className="h-12 w-12" aria-hidden="true"><path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path></svg>
                     </span>
                 </div>
-                <div class="mt-2">
-                    <h3 class="text-base font-semibold leading-6 text-gray-900">
-                        <a href="https://twitter.com/CC_Assoc" target="_blank" rel="noreferrer" class="focus:outline-none">
-                            <span class="absolute inset-0" aria-hidden="true"></span>
+                <div className="mt-2">
+                    <h3 className="text-base font-semibold leading-6 text-gray-900">
+                        <a href="https://twitter.com/CC_Assoc" target="_blank" rel="noreferrer" className="focus:outline-none">
+                            <span className="absolute inset-0" aria-hidden="true"></span>
                             Twitter
                         </a>
                     </h3>
-                    <p class="mt-2 text-sm text-gray-500">Here, you&apos;ll find all the latest updates, news, and educational content from the Crypto Credit Association. Stay informed and discover the world of crypto credit through our engaging videos and insightful tweets.</p>
+                    <p className="mt-2 text-sm text-gray-500">Here, you&apos;ll find all the latest updates, news, and educational content from the Crypto Credit Association. Stay informed and discover the world of crypto credit through our engaging videos and insightful tweets.</p>
                 </div>
-                <span class="pointer-events-none absolute right-6 top-6 text-gray-300 group-hover:text-gray-400" aria-hidden="true">
-                    <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                <span className="pointer-events-none absolute right-6 top-6 text-gray-300 group-hover:text-gray-400" aria-hidden="true">
+                    <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
                         <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
                     </svg>
                 </span>
             </div>
-            <div class="group relative bg-white p-6">
+            <div className="group relative bg-white p-6">
                 <div>
-                    <span class="inline-flex rounded-lg">
-                        <svg class="h-12 w-12" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#5e7591">
+                    <span className="inline-flex rounded-lg">
+                        <svg className="h-12 w-12" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#5e7591">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 18.75a6 6 0 006-6v-1.5m-6 7.5a6 6 0 01-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 01-3-3V4.5a3 3 0 116 0v8.25a3 3 0 01-3 3z" />
                         </svg>
                     </span>
                 </div>
-                <div class="mt-2">
-                    <h3 class="text-base font-semibold leading-6 text-gray-900">
-                        <a href="https://podcasts.apple.com/gb/podcast/crypto-credit-association/id1656660304" target="_blank" rel="noreferrer" class="focus:outline-none">
-                            <span class="absolute inset-0" aria-hidden="true"></span>
+                <div className="mt-2">
+                    <h3 className="text-base font-semibold leading-6 text-gray-900">
+                        <a href="https://podcasts.apple.com/gb/podcast/crypto-credit-association/id1656660304" target="_blank" rel="noreferrer" className="focus:outline-none">
+                            <span className="absolute inset-0" aria-hidden="true"></span>
                             Podcasts
                         </a>
                     </h3>
-                    <p class="mt-2 text-sm text-gray-500">Discover a world of knowledge and captivating discussions through the Crypto Credit Association Podcast. Our podcast series is a treasure trove of insights, interviews, and thought-provoking conversations on all things related to crypto credit, blockchain technology, lending protocols, decentralized finance (DeFi), and more.</p>
+                    <p className="mt-2 text-sm text-gray-500">Discover a world of knowledge and captivating discussions through the Crypto Credit Association Podcast. Our podcast series is a treasure trove of insights, interviews, and thought-provoking conversations on all things related to crypto credit, blockchain technology, lending protocols, decentralized finance (DeFi), and more.</p>
                 </div>
-                <span class="pointer-events-none absolute right-6 top-6 text-gray-300 group-hover:text-gray-400" aria-hidden="true">
-                    <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                <span className="pointer-events-none absolute right-6 top-6 text-gray-300 group-hover:text-gray-400" aria-hidden="true">
+                    <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
                         <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
                     </svg>
                 </span>
             </div>
-            <div class="group relative bg-white p-6">
+            <div className="group relative bg-white p-6">
                 <div>
-                    <span class="inline-flex rounded-lg">
-                        <svg class="h-12 w-12" width="24" height="24" viewBox="0 0 24 24" fill="#5e7591" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="sc-1lvq0y2-4 kBMEHL h-12 w-12"><path d="M12.531 11.294v-.033a.985.985 0 0 1-.021.033h.021z" fill="#5e7591"></path><path d="M20.529 1.945H2.977c-.84 0-1.521.665-1.521 1.485v17.756c0 .82.681 1.485 1.521 1.485H20.53c.841 0 1.522-.666 1.522-1.485V3.43c0-.82-.681-1.485-1.522-1.485zm-12.83 17.35H4.588V9.936h3.11v9.359zM6.143 8.658h-.02c-1.044 0-1.719-.719-1.719-1.617 0-.918.696-1.617 1.76-1.617s1.719.7 1.74 1.617c0 .898-.676 1.617-1.76 1.617zm12.773 10.637h-3.111v-5.008c0-1.258-.45-2.116-1.575-2.116-.86 0-1.372.579-1.597 1.138-.082.2-.102.479-.102.759v5.227h-3.11s.04-8.482 0-9.36h3.11v1.326c.413-.638 1.153-1.546 2.804-1.546 2.046 0 3.581 1.338 3.581 4.213v5.367z" fill="#5e7591"></path></svg>
+                    <span className="inline-flex rounded-lg">
+                        <svg className="h-12 w-12" width="24" height="24" viewBox="0 0 24 24" fill="#5e7591" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" className="sc-1lvq0y2-4 kBMEHL h-12 w-12"><path d="M12.531 11.294v-.033a.985.985 0 0 1-.021.033h.021z" fill="#5e7591"></path><path d="M20.529 1.945H2.977c-.84 0-1.521.665-1.521 1.485v17.756c0 .82.681 1.485 1.521 1.485H20.53c.841 0 1.522-.666 1.522-1.485V3.43c0-.82-.681-1.485-1.522-1.485zm-12.83 17.35H4.588V9.936h3.11v9.359zM6.143 8.658h-.02c-1.044 0-1.719-.719-1.719-1.617 0-.918.696-1.617 1.76-1.617s1.719.7 1.74 1.617c0 .898-.676 1.617-1.76 1.617zm12.773 10.637h-3.111v-5.008c0-1.258-.45-2.116-1.575-2.116-.86 0-1.372.579-1.597 1.138-.082.2-.102.479-.102.759v5.227h-3.11s.04-8.482 0-9.36h3.11v1.326c.413-.638 1.153-1.546 2.804-1.546 2.046 0 3.581 1.338 3.581 4.213v5.367z" fill="#5e7591"></path></svg>
                     </span>
                 </div>
-                <div class="mt-2">
-                    <h3 class="text-base font-semibold leading-6 text-gray-900">
-                        <a href="https://www.linkedin.com/company/crypto-credit-association/" target="_blank" rel="noreferrer" class="focus:outline-none">
-                            <span class="absolute inset-0" aria-hidden="true"></span>
+                <div className="mt-2">
+                    <h3 className="text-base font-semibold leading-6 text-gray-900">
+                        <a href="https://www.linkedin.com/company/crypto-credit-association/" target="_blank" rel="noreferrer" className="focus:outline-none">
+                            <span className="absolute inset-0" aria-hidden="true"></span>
                             Linkedin
                         </a>
                     </h3>
-                    <p class="mt-2 text-sm text-gray-500">The Crypto Credit Association is dedicated to fostering professional connections and industry knowledge in the world of crypto credit. We invite you to join our LinkedIn network—an exclusive platform where you can engage with industry leaders, discover valuable insights, and expand your professional horizons.</p>
+                    <p className="mt-2 text-sm text-gray-500">The Crypto Credit Association is dedicated to fostering professional connections and industry knowledge in the world of crypto credit. We invite you to join our LinkedIn network—an exclusive platform where you can engage with industry leaders, discover valuable insights, and expand your professional horizons.</p>
                 </div>
-                <span class="pointer-events-none absolute right-6 top-6 text-gray-300 group-hover:text-gray-400" aria-hidden="true">
-                    <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                <span className="pointer-events-none absolute right-6 top-6 text-gray-300 group-hover:text-gray-400" aria-hidden="true">
+                    <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
                         <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
                     </svg>
                 </span>
