cfb49f5bd2ddf7c98ce1bab2e1a3cdc29563c7fa
Elektra Murphy <elektra.murphy@icloud.com>
2023-05-29 13:37:52 +0100

    SVG Colour updated to original format

--- pages/media.js
+++ pages/media.js
@@ -28,7 +28,7 @@ function MediaPage() {
                 <div className="rounded-tl-lg sm:rounded-tr-none group relative bg-white p-6">
                   <div className="h-12 w-12">
                     <span className="inline-flex rounded-lg">
-                      <svg fill="#5e7591" height="48px" width="48px" viewBox="0 0 24 24" aria-hidden="true"><path fillRule="evenodd" d="M19.812 5.418c.861.23 1.538.907 1.768 1.768C21.998 8.746 22 12 22 12s0 3.255-.418 4.814a2.504 2.504 0 0 1-1.768 1.768c-1.56.419-7.814.419-7.814.419s-6.255 0-7.814-.419a2.505 2.505 0 0 1-1.768-1.768C2 15.255 2 12 2 12s0-3.255.417-4.814a2.507 2.507 0 0 1 1.768-1.768C5.744 5 11.998 5 11.998 5s6.255 0 7.814.418ZM15.194 12 10 15V9l5.194 3Z" clipRule="evenodd"></path></svg>
+                      <svg fill="#FF0000" height="48px" width="48px" viewBox="0 0 24 24" aria-hidden="true"><path fillRule="evenodd" d="M19.812 5.418c.861.23 1.538.907 1.768 1.768C21.998 8.746 22 12 22 12s0 3.255-.418 4.814a2.504 2.504 0 0 1-1.768 1.768c-1.56.419-7.814.419-7.814.419s-6.255 0-7.814-.419a2.505 2.505 0 0 1-1.768-1.768C2 15.255 2 12 2 12s0-3.255.417-4.814a2.507 2.507 0 0 1 1.768-1.768C5.744 5 11.998 5 11.998 5s6.255 0 7.814.418ZM15.194 12 10 15V9l5.194 3Z" clipRule="evenodd"></path></svg>
                     </span>
                   </div>
                   <div className="mt-2">
@@ -49,7 +49,7 @@ function MediaPage() {
                 <div className="sm:rounded-tr-lg group relative bg-white p-6">
                   <div className="h-12 w-12">
                     <span className="inline-flex rounded-lg">
-                      <svg fill="#5e7591" viewBox="0 0 24 24" height="48px" width="48px" aria-hidden="true"><path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path></svg>
+                      <svg fill="#1DA1F2" viewBox="0 0 24 24" height="48px" width="48px" aria-hidden="true"><path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path></svg>
                     </span>
                   </div>
                   <div className="mt-2">
@@ -70,7 +70,7 @@ function MediaPage() {
                 <div className="group relative bg-white p-6">
                   <div  className="h-12 w-12">
                     <span className="inline-flex rounded-lg">
-                      <svg xmlns="http://www.w3.org/2000/svg" height="48px" width="48px" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#5e7591">
+                      <svg xmlns="http://www.w3.org/2000/svg" height="48px" width="48px" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#7d50df">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 18.75a6 6 0 006-6v-1.5m-6 7.5a6 6 0 01-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 01-3-3V4.5a3 3 0 116 0v8.25a3 3 0 01-3 3z" />
                       </svg>
                     </span>
@@ -93,7 +93,7 @@ function MediaPage() {
                 <div className="group relative bg-white p-6">
                   <div className="h-12 w-12">
                     <span className="inline-flex rounded-lg">
-                      <svg  height="48px" width="48px" viewBox="0 0 24 24" fill="#5e7591" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" className="sc-1lvq0y2-4 kBMEHL h-12 w-12"><path d="M12.531 11.294v-.033a.985.985 0 0 1-.021.033h.021z" fill="#5e7591"></path><path d="M20.529 1.945H2.977c-.84 0-1.521.665-1.521 1.485v17.756c0 .82.681 1.485 1.521 1.485H20.53c.841 0 1.522-.666 1.522-1.485V3.43c0-.82-.681-1.485-1.522-1.485zm-12.83 17.35H4.588V9.936h3.11v9.359zM6.143 8.658h-.02c-1.044 0-1.719-.719-1.719-1.617 0-.918.696-1.617 1.76-1.617s1.719.7 1.74 1.617c0 .898-.676 1.617-1.76 1.617zm12.773 10.637h-3.111v-5.008c0-1.258-.45-2.116-1.575-2.116-.86 0-1.372.579-1.597 1.138-.082.2-.102.479-.102.759v5.227h-3.11s.04-8.482 0-9.36h3.11v1.326c.413-.638 1.153-1.546 2.804-1.546 2.046 0 3.581 1.338 3.581 4.213v5.367z" fill="#5e7591"></path></svg>
+                      <svg  height="48px" width="48px" viewBox="0 0 24 24" fill="#0077b5" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" className="sc-1lvq0y2-4 kBMEHL h-12 w-12"><path d="M12.531 11.294v-.033a.985.985 0 0 1-.021.033h.021z" fill="#5e7591"></path><path d="M20.529 1.945H2.977c-.84 0-1.521.665-1.521 1.485v17.756c0 .82.681 1.485 1.521 1.485H20.53c.841 0 1.522-.666 1.522-1.485V3.43c0-.82-.681-1.485-1.522-1.485zm-12.83 17.35H4.588V9.936h3.11v9.359zM6.143 8.658h-.02c-1.044 0-1.719-.719-1.719-1.617 0-.918.696-1.617 1.76-1.617s1.719.7 1.74 1.617c0 .898-.676 1.617-1.76 1.617zm12.773 10.637h-3.111v-5.008c0-1.258-.45-2.116-1.575-2.116-.86 0-1.372.579-1.597 1.138-.082.2-.102.479-.102.759v5.227h-3.11s.04-8.482 0-9.36h3.11v1.326c.413-.638 1.153-1.546 2.804-1.546 2.046 0 3.581 1.338 3.581 4.213v5.367z" fill="#5e7591"></path></svg>
                     </span>
                   </div>
                   <div className="mt-2">
